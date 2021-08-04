22668d0d68402a310dd4c88406046d3550cdb8c6
menwa <menwa.codes@gmail.com>
2021-08-04 09:36:49 -0400

    Create superuser

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -32,3 +32,12 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'testpw123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            email='test@menwa.com',
+            password='test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
