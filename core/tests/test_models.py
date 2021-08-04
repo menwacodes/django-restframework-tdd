950bff0cb581c23f24cf2132aed10fe4a87c0f77
menwa <menwa.codes@gmail.com>
2021-08-04 09:19:19 -0400

    Email normalized and required

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -16,3 +16,19 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is lower case"""
+        email = 'test@MENWA.COM'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password="123456"
+        )
+
+        self.assertEqual(user.email, user.email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'testpw123')
