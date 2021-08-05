5dbfb7bfc075c158cecabdefb85ebc83982242b4
menwa <menwa.codes@gmail.com>
2021-08-05 09:26:03 -0400

    Tests created for token passing

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -83,7 +83,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertIn("token", res.data)
-        self.assertEqual(res.status, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_invalid_creds(self):
         """Test that token is not created with invalid creds"""
